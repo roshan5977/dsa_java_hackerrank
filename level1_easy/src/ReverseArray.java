305b425468048ad93bfab338db51a6e823badaa5
roshan5977 <roshannayak5977@gmail.com>
2023-09-25 12:55:55 +0530

    java started with qn 1 completed

---
+++ level1_easy/src/ReverseArray.java
@@ -0,0 +1,16 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        System.out.println("hii");
+        int[] arr={2,3,4,5,5,6,3};
+        int[] anotherArr=new int[]{3,4,3,2,};
+        reverseArr(arr);
+
+    }
+
+    public static void reverseArr(int []arr){
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]);
+        }
+
+    }
+}
